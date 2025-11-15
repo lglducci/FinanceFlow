8d325fd04e713441fc0ae0e70f61ef900fceea69
lglducci <lglducci@hotmail.com>
2025-11-15 18:23:05 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-export default function Header() {
+ export default function Header() {
   return (
-    <header className="h-16 bg-white border-b flex items-center justify-between px-6">
+    <header className="h-16 border-b flex items-center justify-between px-6 bg-[#5f5c5d]">
       <div>
-        <h1 className="text-lg font-semibold">Finance-Flow</h1>
-        <p className="text-xs text-gray-500">
+        <h1 className="text-lg font-semibold text-white">Finance-Flow</h1>
+        <p className="text-xs text-gray-200">
           Acompanhamento rápido das suas finanças.
         </p>
       </div>
-      <div className="text-sm text-gray-500">
+      <div className="text-sm text-gray-200">
         {new Date().toLocaleDateString("pt-BR")}
       </div>
     </header>
