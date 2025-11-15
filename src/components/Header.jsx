65aef77d3d8688f1fe05fb962e7202111399f507
lglducci <lglducci@hotmail.com>
2025-11-15 18:28:34 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
  export default function Header() {
   return (
-    <header className="h-16 border-b flex items-center justify-between px-6 bg-[#5f5c5d]">
+    <header className="h-16 border-b flex items-center justify-between px-6 bg-[#2e2ed2]">
       <div>
         <h1 className="text-lg font-semibold text-white">Finance-Flow</h1>
         <p className="text-xs text-gray-200">
